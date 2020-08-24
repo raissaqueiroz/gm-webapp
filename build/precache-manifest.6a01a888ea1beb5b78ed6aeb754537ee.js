5efe7e93382009d781d34adfde8e4df23ac4bac0
Raissa Queiroz <rairfeelas@gmail.com>
2020-08-24 00:49:42 -0300

    add build

---
+++ build/precache-manifest.6a01a888ea1beb5b78ed6aeb754537ee.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b24a22ff38eaf2a14998ff0bca992a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fe52f796bc01ce1bc1c",
+    "url": "/static/css/2.142223b1.chunk.css"
+  },
+  {
+    "revision": "69ec19035cd61789501d",
+    "url": "/static/css/main.b73a57e3.chunk.css"
+  },
+  {
+    "revision": "0fe52f796bc01ce1bc1c",
+    "url": "/static/js/2.b5cfb0b4.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/static/js/2.b5cfb0b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69ec19035cd61789501d",
+    "url": "/static/js/main.193de64e.chunk.js"
+  },
+  {
+    "revision": "f04b6c27bf1b89e04cd7",
+    "url": "/static/js/runtime-main.b94bdb93.js"
+  },
+  {
+    "revision": "b682a42524544f3aadf377adb05d5207",
+    "url": "/static/media/background-contato.b682a425.jpg"
+  },
+  {
+    "revision": "ce1cafe309e8ede8817cf8aad0403dda",
+    "url": "/static/media/background-empresa.ce1cafe3.jpg"
+  },
+  {
+    "revision": "de71f91aced4046a3a56580fe88ee01e",
+    "url": "/static/media/background-portas.de71f91a.jpg"
+  },
+  {
+    "revision": "a778f716c02562759d56d482788a84cf",
+    "url": "/static/media/helvetica.a778f716.ttf"
+  },
+  {
+    "revision": "33091742fdda779e2eef45ebc58ebf35",
+    "url": "/static/media/logo.33091742.png"
+  },
+  {
+    "revision": "e6e22f8ee8cf2229ad91c96233473981",
+    "url": "/static/media/logo_nova.e6e22f8e.png"
+  },
+  {
+    "revision": "e7922de0ab3025adf2088efc21de5516",
+    "url": "/static/media/porta_1.e7922de0.jpg"
+  },
+  {
+    "revision": "e744fc7fc9e7816ca6fcd4a640cbd657",
+    "url": "/static/media/porta_2.e744fc7f.jpg"
+  },
+  {
+    "revision": "eed6c05bda962df515fd4393fe0241ac",
+    "url": "/static/media/porta_3.eed6c05b.jpg"
+  },
+  {
+    "revision": "c4b43d1fef93c506090c3c9f9a6d9f21",
+    "url": "/static/media/segoe-ui.c4b43d1f.otf"
+  },
+  {
+    "revision": "8dadb791f269f7449a11fabd674b879f",
+    "url": "/static/media/servicos.8dadb791.png"
+  },
+  {
+    "revision": "174a9023472f5d12acf07dc66812b1f1",
+    "url": "/static/media/slide_1.174a9023.jpg"
+  }
+]);
