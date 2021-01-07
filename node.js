27cb3b7c5a4ad964596c8e310cf5eed1c40b14c1
Raissa Queiroz <raifreelas@gmail.com>
2021-01-07 06:05:15 -0300

    Feature: active https default in production

--- node.js
+++ node.js
@@ -4,7 +4,7 @@ const app = express();
 
 const configs = {
     caminho: "build", //Aqui será definido a pasta de saída onde contém o index.html e os outros arquivos.
-    forcarHTTPS: false, //Defina para true se desejar que o redirecionamento para HTTPS seja forçado (é necessário certificado SSL ativo)
+    forcarHTTPS: true, //Defina para true se desejar que o redirecionamento para HTTPS seja forçado (é necessário certificado SSL ativo)
     port: process.env.PORT || 3000
 }
 
